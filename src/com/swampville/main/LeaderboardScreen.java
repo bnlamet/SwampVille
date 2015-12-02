e5c9cd020b424f395613d41c9c2736683352a8d4
Foster Clark <Fclark84@gmail.com>
2015-12-01 20:24:03 -0500

    Leaderboard is updated
    Spacing isn’t quite right, but displays all current scores.

--- src/com/swampville/main/LeaderboardScreen.java
+++ src/com/swampville/main/LeaderboardScreen.java
@@ -1,10 +1,16 @@
 package com.swampville.main;
+import java.util.Arrays;
 
+import java.awt.Font;
 import java.awt.event.ActionEvent;
+import java.io.*;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextPane;
+
+import net.miginfocom.swing.MigLayout;
 
 /**
  * @author Mark Betters
@@ -18,7 +24,7 @@ public class LeaderboardScreen implements ActionListener {
 
 	JFrame frame;
 	
-	int[] currentTopScores;
+	int[] currentTopScores = {0,0,0}; //scores from highest to lowest
 	
 	int totalScoreOfPlayer;
 	
@@ -30,15 +36,130 @@ public class LeaderboardScreen implements ActionListener {
 	 */
 	public LeaderboardScreen(JFrame frame, int totalScoreOfPlayer) {
 		this.frame = frame;
-		this.askForInitials();
+	//	this.askForInitials();
 		this.totalScoreOfPlayer = totalScoreOfPlayer;
+		this.displayScores();
 	}
+	
+
+	
+	public void displayScores(){
+		int x = currentTopScores[0];
+		int y = currentTopScores[1];
+		int z = currentTopScores[2];
+		
+		//String scores = Arrays.toString(currentTopScores);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setBounds(100, 100, 1000, 700);
+		
+	    frame.getContentPane().setLayout(new MigLayout("", "[grow]", "[grow][grow][]"));
+		
+	    //Scores
+	    JTextPane topThree = new JTextPane();
+		topThree.setFont(new Font("Tahoma", Font.PLAIN, 31));
+		topThree.setEditable(false);
+		topThree.setText("Top Three Scores");
+		frame.getContentPane().add(topThree, "cell 0 0,alignx center, wrap");
+
+		//Highest Score
+		JTextPane scoreOne = new JTextPane();
+		scoreOne.setFont(new Font("Tahoma", Font.PLAIN, 31));
+		scoreOne.setEditable(false);;
+		scoreOne.setText(Integer.toString(x));
+		frame.getContentPane().add(scoreOne, "cell 0 1,alignx center,wrap");
+
+		//Second Highest Score
+		JTextPane scoreTwo = new JTextPane();
+		scoreTwo.setFont(new Font("Tahoma", Font.PLAIN, 31));
+		scoreTwo.setEditable(false);;
+		scoreTwo.setText(Integer.toString(y));
+		frame.getContentPane().add(scoreTwo, "cell 0 2,alignx center,wrap");
+
+		//Second Highest Score
+		JTextPane scoreThree = new JTextPane();
+		scoreThree.setFont(new Font("Tahoma", Font.PLAIN, 31));
+		scoreThree.setEditable(false);;
+		scoreThree.setText(Integer.toString(z));
+		frame.getContentPane().add(scoreThree, "cell 0 3,alignx center,wrap");
+
+		
+		//adding back button
+		JButton btnNewButton = new JButton("Go to Start Screen");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				transitionToTitleScreen();
+			}});
+		frame.getContentPane().add(btnNewButton, "cell 0 4,alignx center,wrap");
+		frame.getContentPane().revalidate();
+	}
+	
+	
+	
+	int[] updateScores(){
+		//for(int i=0; i < currentTopScores.length; i++){
+			if(totalScoreOfPlayer > currentTopScores[0]){
+				currentTopScores[2] = currentTopScores[1];
+				currentTopScores[1] = currentTopScores[0];
+				currentTopScores[0] = totalScoreOfPlayer;
+			}
+			if(totalScoreOfPlayer > currentTopScores[1]){
+				currentTopScores[2] = currentTopScores[1];
+				currentTopScores[1] = totalScoreOfPlayer;
+			}
+			if(totalScoreOfPlayer > currentTopScores[2]){
+				currentTopScores[2] = totalScoreOfPlayer;
+			}
+			
+		//}
+		
+		
+		return(currentTopScores);
+	}
+	
 	
 	/**
 	 * 		TODO: Use Serializable here.
 	 */
 	public void readCurrentTopScoresFromAppMemory() {
+	      int[] e = null;
+	      try
+	      {
+	         FileInputStream fileIn = new FileInputStream("/tmp/scores.ser");
+	         ObjectInputStream in = new ObjectInputStream(fileIn);
+	         e = (int[]) in.readObject();
+	         in.close();
+	         fileIn.close();
+	      }catch(IOException i)
+	      {
+	         i.printStackTrace();
+	         return;
+	      }catch(ClassNotFoundException c)
+	      {
+	         System.out.println("Scores not found");
+	         c.printStackTrace();
+	         return;
+	      }
+	      currentTopScores = e;
+	      updateScores();
 
+	}
+	
+	public void saveCurrentScores(){
+		int[] e = currentTopScores; 
+	      try
+	      {
+	         FileOutputStream fileOut = new FileOutputStream("/tmp/scores.ser");
+	         ObjectOutputStream out = new ObjectOutputStream(fileOut);
+	         out.writeObject(e);
+	         out.close();
+	         fileOut.close();
+	         System.out.printf("Serialized data is saved in /tmp/scores.ser");
+	      }catch(IOException i)
+	      {
+	          i.printStackTrace();
+	      }
+
+		
 	}
 	
 	/**
@@ -66,6 +187,9 @@ public class LeaderboardScreen implements ActionListener {
 	 * 		pass frame to title screen
 	 */
 	public void transitionToTitleScreen() {
+		frame.getContentPane().removeAll();
+		frame.getContentPane().repaint();
+		new TitleScreen(frame);
 
 	}
 
